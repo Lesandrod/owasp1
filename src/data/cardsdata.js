a4ebed2984ae724f6240370250fcba223b9b1b6a
Lesandrod <lentrix12345@gmail.com>
2023-11-14 05:58:51 -0500

    la pichicata es buena

---
+++ src/data/cardsdata.js
@@ -0,0 +1,54 @@
+const cards = [
+    {
+        img: "https://i.ibb.co/HqgT30f/TOP-10-Icons-Final-Broken-Access-Control.png",
+        name: "A01:2021 ",
+        description: "Pérdida de Control de Acceso"
+    },
+    {
+        img: "https://i.ibb.co/zrcCyB7/TOP-10-Icons-Final-Crypto-Failures-1.png",
+        name: "A02:2021",
+        description: "Fallas Criptográficas"
+    },
+    {
+        img: "https://i.ibb.co/M8DR88Z/TOP-10-Icons-Final-Injection.png",
+        name: "A03:2021  ",
+        description: "Inyección"
+    },
+    {
+        img: "https://i.ibb.co/bBhDGtS/TOP-10-Icons-Final-Insecure-Design.png",
+        name: "A04:2021 ",
+        description: "Diseño Inseguro"
+    },
+    {
+        img: "https://i.ibb.co/SwCQyKf/TOP-10-Icons-Final-Security-Misconfiguration.png",
+        name: "A05:2021 ",
+        description: "Configuración de Seguridad Incorrecta"
+    },
+    {
+        img: "https://i.ibb.co/FV0kkQV/TOP-10-Icons-Final-Vulnerable-Outdated-Components.png",
+        name: "A06:2021",
+        description: "Componentes Vulnerables y Desactualizados"
+    },
+    {
+        img: "https://i.ibb.co/hs4b8Yv/TOP-10-Icons-Final-Identification-and-Authentication-Failures.png",
+        name: "A07:2021 ",
+        description: "Fallas de Identificación y Autenticación"
+    },
+    {
+        img: "https://i.ibb.co/VQRZRCj/TOP-10-Icons-Final-Software-and-Data-Integrity-Failures.png",
+        name: "A08:2021 ",
+        description: "Fallas en el Software y en la Integridad de los Datos"
+    },
+    {
+        img: "https://i.ibb.co/FVHM8P3/TOP-10-Icons-Final-Security-Logging-and-Monitoring-Failures.png",
+        name: "A09:2021",
+        description: "Fallas en el Registro y Monitoreo"
+    },
+    {
+        img: "https://i.ibb.co/yQG3BPC/TOP-10-Icons-Final-SSRF.png",
+        name: "A10:2021",
+        description: "Falsificación de Solicitudes del Lado del Servidor"
+    }
+]
+
+export default cards;
